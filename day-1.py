80bc5d3e56efe7cfc79cc57545f9b7e4f15f5394
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-21 23:56:11 +0545

    The revision of string methods.

---
+++ day-1.py
@@ -0,0 +1,8 @@
+# Revison of string methods
+
+my_string = "nepal is my country."
+print(my_string.capitalize()) # Converts the first character to upper case
+
+name = 'Subash'
+print(my_string.casefold()) # converts string into lower case
+print(name.casefold())
