fb328fbc1e2848a8367e34ecfef3229fe15a4b46
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-26 23:43:35 +0545

    append and extend.

--- lists1.py
+++ lists1.py
@@ -13,3 +13,22 @@ print(names)
 
 fruits1 = fruits.copy()
 print(fruits1)
+
+# syntax : list.count(value)
+fruits.append('apple')
+fruits.append('apple')
+print(fruits)
+# The count() method returns the number of elements with the specified value.
+print(fruits.count('apple'))
+print('\n')
+
+# syntax : list.extend(iterable)
+# any iterable, list, set, tuple, etc.
+# The extend() method adds the specified list elements (or any iterable) to the end of the current list.
+points = (1,4,7,8)
+fruits.extend(points)
+print(fruits)
+
+fruits.extend('nepal')
+print(fruits)
+
