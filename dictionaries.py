7644a183b0b51b252625af58dfd2fce029d73ca5
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-27 23:19:43 +0545

    fromkeys method

--- dictionaries.py
+++ dictionaries.py
@@ -19,5 +19,24 @@ car = {
 }
 
 x = car.copy()
-
 print(x)
+
+# syntax : dict.fromkeys(keys, value)
+# keys : an iterable specifying the keys of the new dictionary
+# value : opt, the value for all keys, default is None
+
+x = ('key1', 'key2', 'key3')
+thisdict = dict.fromkeys(x)
+print(thisdict)
+print('\n')
+
+# with values
+values = [12, 45, 'Nepal']
+thisdict = dict.fromkeys(x, values)
+print(thisdict)
+print('\n')
+
+# another
+values = 'Nepal'
+thisdict = dict.fromkeys(x, values)
+print(thisdict)
