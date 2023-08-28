7d33a54f23c58c20f8c4e882eb1648e4ba3e6292
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-28 23:22:40 +0545

    set methods.

---
+++ sets1.py
@@ -0,0 +1,9 @@
+# Let's have a look into set methods
+fruits = {'apple', 'banana', 'cherry'}
+print(fruits)
+fruits.add('orange')
+print(fruits)
+print('')
+
+fruits.clear() # removes all elements in a set
+print(fruits)
