89bb24db0daac1713c7323d58067affe434b9bc2
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-28 23:03:50 +0545

    tuples and methods.

---
+++ tuples1.py
@@ -0,0 +1,10 @@
+# syntax : tuple.count(value)
+# value : the item to search for
+# returns the number of times a specified value appears in the tuple.
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 5, 6, 7)
+x = numbers.count(7)
+print(x)
+
+# syntax : tuple.index(value)
+# finds the first occurence of the specified value
+# raise exception if the value is not found.
