b16ed324f41d2d5f24c30170b784ad98a6bcb586
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-29 22:49:10 +0545

    set copy and difference.

--- sets1.py
+++ sets1.py
@@ -7,3 +7,19 @@ print('')
 
 fruits.clear() # removes all elements in a set
 print(fruits)
+
+
+fruits1 = {'mango', 'banana', 'pear', 'kiwi'}
+fp = fruits.copy()
+print(fp)
+
+# set.difference(set)
+# set : the set to check the difference in
+# the returned set contains items that exists only in the first set,
+# and not in both sets.
+fruits = {'apple', 'banana', 'cherry'}
+x = fruits.difference(fruits1)
+print(x)
+
+x = fruits1.difference(fruits)
+print(x)
