bfeb73847c0487418ab6e9f87ed1f4c2c138eac0
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-31 23:36:47 +0545

    close and file no

--- file_methods.py
+++ file_methods.py
@@ -1,1 +1,15 @@
 # File methods
+# file.close()
+fp = open("data.txt", "r")
+print(fp.read())
+fp.close()
+print('\n')
+# closes an open file
+
+# detach() : returns the separated raw stream from the buffer
+# file.fileno()
+# returns the file description of the stream, as a number
+# an error will occur if the operator system does not use file descriptor
+
+fp = open('data.txt', 'r')
+print(fp.fileno())
