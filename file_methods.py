fe58724f63cd9d7a7a9d6b061050b610867d3a4e
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-31 23:50:31 +0545

    read method.

--- file_methods.py
+++ file_methods.py
@@ -13,3 +13,30 @@ print('\n')
 
 fp = open('data.txt', 'r')
 print(fp.fileno())
+
+# file.flush()
+fp = open('data.txt', 'a')
+fp.write('This is the new file.\n')
+fp.flush()
+fp.write('This is the second new line from script\n')
+
+print('\n')
+
+# file.isatty()
+# returns True if the file stream is interactive, example: connected 
+# to a terminal device
+fp = open('data.txt', 'r')
+print(fp.isatty())
+print('')
+
+# file.read()
+# returns the specified number of bytes from the file. Default is -1 
+# which means the whole file
+fp = open('data.txt', 'r')
+print(fp.read())
+print('')
+
+print('Reading given number of bytes: ')
+fp = open('data.txt', 'r')
+print(fp.read(50))
+print('')
