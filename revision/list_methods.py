5bf13a7e7908fecd17ed36da95884a67f61ac4c4
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-15 23:26:41 +0545

    list methods revised.

---
+++ revision/list_methods.py
@@ -0,0 +1,10 @@
+names = ['apple', 'banana', 'coconut', 'grapes']
+print(names)
+names.append('mango')
+print(names)
+fruits = ['banana', 'cherry']
+names.extend(fruits)
+print(names)
+print(names.count('apple'))
+
+print(names.index('grapes'))
