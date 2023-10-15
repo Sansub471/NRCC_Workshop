8399897060396fe86314c1d0110e655fc0857fed
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-15 23:31:55 +0545

    list methods done ..

--- revision/list_methods.py
+++ revision/list_methods.py
@@ -8,3 +8,17 @@ print(names)
 print(names.count('apple'))
 
 print(names.index('grapes'))
+
+print(names.insert(2, 'mulberry'))
+print(names)
+
+print(names.pop(3)) # removes the item at position and returns it 
+
+names.reverse()
+print(names)
+
+def sort_func(elem):
+    return len(elem)
+
+names.sort(reverse=False, key=sort_func)
+print(names)
