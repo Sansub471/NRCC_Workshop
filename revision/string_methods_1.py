c4bc4c20fc1668aa8709ed7f95f5cb184a248b67
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-02 22:41:08 +0545

    Revision of string methods started.

---
+++ revision/string_methods_1.py
@@ -0,0 +1,9 @@
+# Revision of string methods
+my_string = "nepal is my country"
+# string.capitalize() # Converts the first character to upper case
+# string.casefold() # Converts string into lower case
+
+country = 'NEPAL'
+print(country)
+print(country.casefold())
+# string.center(length, character)
