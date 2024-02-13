f5cc7445331b1ca3989f818d88ae2464343d00fd
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-13 23:52:00 +0545

    String revision two.

--- revision/string_methods_1.py
+++ revision/string_methods_1.py
@@ -16,3 +16,20 @@ print(country.center(20, '*'))
 print("string.count(value, start, end) : ")
 print(country.count('N'))
 print(my_string.count('y', 5))
+
+# string.encode(encoding=encoding, errors = errors)
+
+# string.endswith(value, start, end)
+# returns true or false
+ioe = 'https:://exam.ioe.edu.np'
+tu = 'https:://tu.exam.edu.np'
+print("string.endswith(value, start, end)")
+print(ioe.endswith('.np'))
+print(tu.endswith('.np'))
+
+# string.find(value, start, end)
+# value : required value to search for in the string. -1 means not found
+my_string2 = 'Nepal is a small country in South Asia between two giants China and India.'
+print("string.find(value, start, end) function : ")
+print(my_string2.find('America')) # not found means -1
+print(my_string2.find('China')) # finds the first occurence
