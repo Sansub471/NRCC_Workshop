08f1e0ba9cf8798effbbe5155eb781fa58cda5a8
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-13 22:44:02 +0545

    Revison of string methods one.

--- revision/string_methods_1.py
+++ revision/string_methods_1.py
@@ -7,3 +7,12 @@ country = 'NEPAL'
 print(country)
 print(country.casefold())
 # string.center(length, character)
+# length : the length of the required string
+# character : opt, the character to fill the missing space on each side 
+# using a specified character (default space) as the fill character
+print(country.center(20, '*'))
+
+# string.count(value, start, end)
+print("string.count(value, start, end) : ")
+print(country.count('N'))
+print(my_string.count('y', 5))
