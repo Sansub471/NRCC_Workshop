204c6271182b8326fb5a7a8e44d3fc40b676d328
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-20 23:48:37 +0545

    Repeated lines removed.

--- revision/string_methods_1.py
+++ revision/string_methods_1.py
@@ -158,11 +158,3 @@ print('\n')
 
 # Make trans function 
 # string.maketrans(x, y, z)
-txt = ",,,,,ssaawwfgk...mango..banana"
-x = txt.lstrip(",saw.") # characters to remove given
-print(x)
-print('\n')
-
-
-# Make trans function 
-# string.maketrans(x, y, z)
