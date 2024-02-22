1430176143e17adf421bde35b926eb7da1620027
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-22 22:25:29 +0545

    partition method.

---
+++ revision/string_methods_2.py
@@ -0,0 +1,11 @@
+# syntax : string.partition(value)
+# value : first occurence of string to search for
+
+# splits the string into a tuple containing three elements.
+
+# part before the 'value' string, the 'value' string and the part after 
+# the 'value' string.
+
+info = "I could eat mangos all day."
+print(info.partition('mangos'))
+print('\n')
