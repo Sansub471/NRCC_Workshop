272553550fb693c5afad79783f6f17e9c93e44ab
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-22 23:44:40 +0545

    rfind and rinde functions.

--- revision/string_methods_2.py
+++ revision/string_methods_2.py
@@ -9,3 +9,24 @@
 info = "I could eat mangos all day."
 print(info.partition('mangos'))
 print('\n')
+
+# string.replace(oldvalue, newvalue, count)
+# count : how many occurence to replace, default is all.
+
+txt = "one one was a race horse, two two was one too."
+x = txt.replace("one", "three", 2)
+print(x)
+
+# string.rfind(value, start, end)
+# finds the last occurence of the specified value
+# returns -1 if the value is not found
+
+
+x = txt.rfind("one", 8, -1)
+print(x)
+
+# string.rindex(value, start, end)
+# find the last occurence of the specified value. 
+# an exception is raised if not found
+x = txt.index("one", 8, -1)
+print(x)
