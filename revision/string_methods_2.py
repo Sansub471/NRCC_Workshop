0162585e80ca4c296cb11a31caab7253266f40b4
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-23 23:37:11 +0545

    split and strip functions.

--- revision/string_methods_2.py
+++ revision/string_methods_2.py
@@ -28,7 +28,7 @@ print(x)
 # string.rindex(value, start, end)
 # find the last occurence of the specified value. 
 # an exception is raised if not found
-x = txt.index("one", 8, -1)
+x = txt.rindex("one", 8, -1)
 print(x)
 
 # string.rjust(length, character)
@@ -46,4 +46,27 @@ print(txt.rjust(20, '#'))
 txt = 'nepal is in south asia. nepal is next to india and china, nepal is my home'
 print(txt.rpartition('nepal'))
 
+# string.rsplit(separator, maxsplit)
+# splits the string into a list, starting from the right
+# separator : opt, default is space
+# maxsplit : opt, how many splits, default is -1
 
+print("rsplit() function : ")
+txt = 'mango, apple, cherry, banana, pineapple, peach'
+x = txt.rsplit(',', 2) # max length 2 + 1
+print(x) # -1 is all, empty is also all
+# If maxsplit is specified, the list will have the maximum of maxsplit+1 items.
+print(f'The lenght of split is : {len(x)}')
+
+# string.rstrip(characters)
+# remove trailing characters from a string
+# characters : opt, default is space, set of characters to remove
+txt = "         nepal is my country.    @@@@***$!!!!!%@#"
+print(txt.rstrip('@!*$%#'))
+
+# string.split(separator, maxsplit)
+# splits a string into a list
+txt = "hello, my name is Subash Mainali, I'm currently unemployed right now."
+x = txt.split(",", 1) # the list will have maximum of maxsplit + 1 elements
+# -1 or empty means all
+print(x)
