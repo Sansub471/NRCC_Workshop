62a29fe0b0d149d4188d0e25535e6edd33c40f5c
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-27 22:19:52 +0545

    file.read() function.

--- revision/file_methods_1.py
+++ revision/file_methods_1.py
@@ -5,3 +5,21 @@ print(fp.read())
 fp.close()
 print("\n")
 # Closes an opened file
+
+# file.read()
+# returns the specified number of bytes from the file. Default is -1
+# which means the whole file.
+
+fp = open(source, 'r')
+lines = fp.read() # Read the entire file
+print(lines)
+print("\n")
+fp.close()
+
+# Read 200 bytes from file
+fp = open(source, 'r')
+byte_size = 39
+lines = fp.read(byte_size)
+print(lines)
+print('\n')
+
